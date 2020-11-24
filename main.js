008c68e6ea54fba19ae8379d2a2e37cf0b2e8852
Alejandro879 <luis.87914552@gmail.com>
2020-11-24 01:13:11 -0300

    fix 2

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ Chart.defaults.global.defaultFontFamily = "Lato";
 Chart.defaults.global.defaultFontSize = 18;
 
 var speedData = {
-  labels: ["01-10", "02-10", "03-10", "04-10", "05-10", "06-10", "07-10", "08-10", "09-10", "10-10", "11-10"],
+  labels: ["01", "02", "03", "04", "05"],
   datasets: [{
-    data: [10, 29, 35, 40, 54, 45, 64, 73, 80, 100, 108],
-    backgroundColor: 'rgb(54, 162, 235, 0.8)',
+    data: [ 29, 35, 40, 54,42],
+    backgroundColor: [
+    'rgb(54, 162, 235, 0.8)', 'rgb(224, 162, 235, 0.8)', 'rgb(255, 193, 7)',
+    'rgb(144, 155, 213, 0.8)','rgb(54, 62, 235, 0.8)','rgb(248, 229, 91, 0.8)'],
+    label: 'Dataset  1'
     
   }]
 };
@@ -17,18 +20,22 @@ var speedData = {
 var chartOptions = {
   responsive: true,
   legend: {
-    display: false,
-    position: 'top',
+    display: true,
+    position: 'left',
     labels: {
       boxWidth: 100,
       fontColor: 'black'
     }
   },
+  animation: {
+          animateScale: true,
+          animateRotate: true
+        }
   
 };
 
 var lineChart = new Chart(speedCanvas, {
-  type: 'line',
+  type: 'doughnut',
   data: speedData,
   options: chartOptions
 });
